28426a480850b4448b7dbd15d09631f19883b652
Cassiano Ramos <cassianolori5@gmail.com>
2025-08-22 09:02:41 -0300

    HEADER

--- front-end/index.php
+++ front-end/index.php
@@ -8,7 +8,29 @@
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <header></header>
+<header>
+    <div class="top-bar">
+      <div class="top-links">
+        <a href="#">Minha conta</a>
+        <a href="#">Pedidos</a>
+        <a href="#">Ajuda</a>
+      </div>
+    </div>
+
+    
+    <div class="main-bar">
+      <div class="logo">americanas</div>
+      <div class="search-box">
+        <input type="text" placeholder="Buscar produtos..." />
+        <button><i class="fas fa-search"></i></button>
+      </div>
+      <div class="icons">
+        <a href="#"><i class="fas fa-heart"></i></a>
+        <a href="#"><i class="fas fa-user"></i></a>
+        <a href="#"><i class="fas fa-shopping-cart"></i></a>
+      </div>
+    </div>
+  </header>
     <main></main>
 
     <footer></footer>

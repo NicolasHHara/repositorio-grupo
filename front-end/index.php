22777ba042c7ac736375f327d0c3427c063bf339
Nicolas <nico.hhara@gmail.com>
2025-08-22 09:54:52 -0300

    sdasdad

--- front-end/index.php
+++ front-end/index.php
@@ -1,18 +1,29 @@
+<?php
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <script src="script.js"></script>
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
     <header></header>
-    <main></main>
+    <main id="main">
+        <?php
+        for ($i = 0; $i < 4; $i++) {
+            echo '<button class="button-main">
+                    <div class="container-main">
+                        <img src="#" alt="" class="img-main">
+                        <h1 class="title-main">Título ' . ($i+1) . '</h1>
+                        <p class="paragraph-main">Parágrafo ' . ($i+1) . '</p>
+                    </div>
+                  </button>';
+        }
+        ?>
+    </main>
     <footer></footer>
 </body>
 </html>
-<?php
-
-?>
